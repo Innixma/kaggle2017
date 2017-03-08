99d64728b6ba154a0a3222d591f6932d0796c78b
bhillmann <bhillmann@outlook.com>
2017-03-07 23:19:23 -0600

    Added the preprocess script.

--- scripts/template_script.py
+++ scripts/template_script.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+import argparse
+
 DESCRIPTION = """
 Explain the script here
 """
-import argparse
+
 
 def make_arg_parser():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('-i', '--input', help='<PATH> The input folder', type=str, required=True)
     parser.add_argument('-o', '--output', help='<PATH> The output folder', type=str, required=True)
     return parser
+
 
 # Driver function
 def main():
