c82399536aa9f45a688711784b35e1be4204e36f
bhillmann <bhillmann@outlook.com>
2017-04-06 12:09:06 -0500

    Fixed the bugs and added the save model feature.

--- scripts/predict_cnn.py
+++ scripts/predict_cnn.py
@@ -6,9 +6,10 @@ import random
 random.seed(1337)
 np.random.seed(1337)  # for reproducibility
 
-from keras.models import Sequential
+from keras.models import Sequential, load_model, save_model
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution3D, MaxPooling3D
+from keras.utils import np_utils
 import argparse
 import os
 
@@ -18,7 +19,7 @@ DESCRIPTION = """
 Runs a CNN on the offline preprocessed lung data
 """
 
-BATCH_SIZE = 10
+BATCH_SIZE = 1
 NB_CLASSES = 2
 NB_EPOCH = 3
 
@@ -29,28 +30,54 @@ NB_FILTERS = 32
 # size of pooling area for max pooling
 POOL_SIZE = (2, 2, 2)
 # convolution kernel size
-KERNEL_SIZE = (3, 3, 3)
+KERNEL_SIZE = (5, 5, 5)
 
 
 def make_arg_parser():
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('-i', '--input', help='<PATH> The input folder', type=str, required=True)
     parser.add_argument('-m', '--mapping_file', help='<PATH> To the sample mapping file', type=str, required=True)
+    parser.add_argument('-s', '--save', help='<Path> to save the model', type=str, required=True)
     return parser
 
 
-def generate_arrays_from_file(training_set, input_folder):
-    while 1:
-        rows = training_set.ix[random.sample(training_set.index, BATCH_SIZE)]
-        X = []
-        for i, row in rows.iterrows():
-            mat_pth = os.path.join(input_folder, "%s.npz" % row['id'])
+def generate_training_set(training_set, input_folder):
+    batch_samples = np.zeros((BATCH_SIZE, INPUT_SHAPE[0], INPUT_SHAPE[1], INPUT_SHAPE[2], INPUT_SHAPE[3]))
+    batch_features = np.zeros((BATCH_SIZE))
+
+    true_postive_set = training_set['cancer'] == 1
+    x = 0
+    while True:
+        for i in range(BATCH_SIZE):
+            if random.uniform(0, 1) >= .5:
+                index = random.choice(training_set[true_postive_set].index)
+            else:
+                index = random.choice(training_set[~true_postive_set].index)
+            mat_pth = os.path.join(input_folder, "%s.npz" % training_set.ix[index]['id'])
             img = load(mat_pth).astype(np.float32)
-            img = img.reshape(*INPUT_SHAPE)
-            X.append(img)
-        X = np.array(X).reshape(BATCH_SIZE, *INPUT_SHAPE)
-        print(X.shape)
-        yield (X, rows['cancer'].values.astype(np.int8).reshape(BATCH_SIZE))
+            img = img.reshape(1, *INPUT_SHAPE)
+            batch_samples[i] = img
+            batch_features[i] = training_set.ix[index]['cancer']
+        batch_features = np_utils.to_categorical(batch_features.astype(np.int), NB_CLASSES)
+        x += 1
+        print(x)
+        yield (batch_samples.astype(np.float32), batch_features)
+
+
+def generate_test_set(test_set, input_folder):
+    x = 0
+    print(test_set['cancer'])
+    while True:
+        for i, row in test_set.iterrows():
+            mat_pth = os.path.join(input_folder, "%s.npz" % row['id'])
+            sample = load(mat_pth).astype(np.float32)
+            sample = sample.reshape(1, *INPUT_SHAPE)
+            feature = np.zeros((1))
+            feature[0] = row['cancer']
+            feature = np_utils.to_categorical(feature.astype(np.int), NB_CLASSES)
+            x += 1
+            print(x)
+            yield (sample, feature)
 
 
 def build_network():
@@ -70,10 +97,10 @@ def build_network():
     model.add(Dense(8))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(1))
+    model.add(Dense(NB_CLASSES))
     model.add(Activation('softmax'))
 
-    model.compile(loss='binary_crossentropy',
+    model.compile(loss='categorical_crossentropy',
                   optimizer='adadelta',
                   metrics=['accuracy'])
 
@@ -89,15 +116,25 @@ def main():
     infiles = os.listdir(args.input)
     infiles = [infile[:-4] for infile in infiles]
 
-    training_set = df_mapping[df_mapping['id'].isin(infiles)]
-    # for (train, data) in generate_arrays_from_file(training_set, args.input):
-    #     print(train)
-    model = build_network()
-    # print(model.summary())
-    #
-    model.fit_generator(generate_arrays_from_file(training_set, args.input), BATCH_SIZE, NB_EPOCH)
-    #
-    # df_mapping[]
+    all_files = df_mapping[df_mapping['id'].isin(infiles)]
+
+    training_mask = np.zeros((all_files.shape[0]))
+
+    training_mask[np.random.uniform(0, 1, all_files.shape[0]) <= .9] = 1
+    training_mask = training_mask.astype(bool)
+
+    if os.path.exists(args.save):
+        model = load_model(args.save)
+    else:
+        model = build_network()
+        model.fit_generator(generate_training_set(all_files[training_mask], args.input), int(len(infiles)/BATCH_SIZE), NB_EPOCH)
+        save_model(model, args.save, overwrite=True)
+
+    score = model.evaluate_generator(generate_test_set(all_files[~training_mask], args.input), 250)
+
+    print(model.predict_generator(generate_test_set(all_files[~training_mask], args.input), 250))
+    print('Test score:', score[0])
+    print('Test accuracy:', score[1])
 
 # Used for thread safety
 if __name__ == '__main__':
